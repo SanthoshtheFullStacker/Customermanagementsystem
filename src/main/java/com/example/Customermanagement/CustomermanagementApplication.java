0561b09fce95ad08571eeb8ed08fc0126eee78d0
Santhoshkumar <santhoshvelmurugan07@gmail.com>
2025-09-07 13:18:59 +0530

    Database setup and default user login setup done

--- src/main/java/com/example/Customermanagement/CustomermanagementApplication.java
+++ src/main/java/com/example/Customermanagement/CustomermanagementApplication.java
@@ -2,12 +2,27 @@ package com.example.Customermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
+@RestController
 public class CustomermanagementApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(CustomermanagementApplication.class, args);
+
+        SpringApplication.run(CustomermanagementApplication.class, args);
 	}
+
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "server running on the port 8099";
+    }
+
+
 
 }
